92f4fd777a3f6b04660e370049d8e5e7efc6214d
Matthew Purcell <mattpurc@amazon.com>
2022-05-30 14:35:16 +1000

    S3 listing update

--- s3-list-buckets/s3-list-buckets.py
+++ s3-list-buckets/s3-list-buckets.py
@@ -3,13 +3,10 @@ import logging
 
 # Obtain references to AWS services
 s3 = boto3.resource('s3')
-sns = boto3.resource('sns')
 
 # List the bucket names
-buffer = ""
 for bucket in s3.buckets.all():
-    buffer += bucket.name + ", " + bucket.creation_date.strftime("%B %d, %Y") + "\n"
-
-print(buffer)
+    bucketName = bucket.name + ", " + bucket.creation_date.strftime("%B %d, %Y") + "\n"
+    print(bucketName)
 
 quit()
